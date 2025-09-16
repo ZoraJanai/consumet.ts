f271f97d43d3d3288e1d50f58f565a5813e6c150
ZoraJanai <72376879+ZoraJanai@users.noreply.github.com>
2025-09-17 02:02:39 +0930

    Update kwik.js

--- dist/extractors/kwik.js
+++ dist/extractors/kwik.js
@@ -12,11 +12,14 @@ class Kwik extends models_1.VideoExtractor {
                 const { data } = await this.client.get(`${videoUrl.href}`, {
                     headers: { Referer: this.host },
                 });
-                const source = eval(/(eval)(\(f.*?)(\n<\/script>)/s.exec(data)[2].replace('eval', '')).match(/https.*?m3u8/);
-                this.sources.push({
-                    url: source[0],
-                    isM3U8: source[0].includes('.m3u8'),
-                });
+
+                this.sources.push(data)
+
+                //const source = eval(/(eval)(\(f.*?)(\n<\/script>)/s.exec(data)[2].replace('eval', '')).match(/https.*?m3u8/);
+                //this.sources.push({
+                //    url: source[0],
+                //    isM3U8: source[0].includes('.m3u8'),
+                //});
                 return this.sources;
             }
             catch (err) {
