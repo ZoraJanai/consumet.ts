e5cb63e778edca722187bba6914df344f86751a1
ZoraJanai <72376879+ZoraJanai@users.noreply.github.com>
2025-09-17 00:31:56 +0930

    updated cookies for animepahe

--- src/providers/anime/animepahe.ts
+++ src/providers/anime/animepahe.ts
@@ -234,7 +234,7 @@ class AnimePahe extends AnimeParser {
       authority: 'animepahe.si',
       accept: 'application/json, text/javascript, */*; q=0.01',
       'accept-language': 'en-US,en;q=0.9',
-      cookie: '__ddg2_=;',
+      cookie: '__ddgid_=; __ddgmark_=; __ddg2_=; __ddg1_=; ddg_last_challenge=; aud=jpn; av1=0; __ddg9_=; __ddg8_=; __ddg10_=',
       dnt: '1',
       'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
       'sec-ch-ua-mobile': '?0',
